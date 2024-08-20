5cbb1fba3663ed92a98299a90841862d60a542e8
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-20 16:35:51 +0530

    Hover effect to category card

--- client/src/components/ui/Categorycard.tsx
+++ client/src/components/ui/Categorycard.tsx
@@ -9,9 +9,9 @@ type CategorycardProps = {
 
 const Categorycard = ({category,icon}:CategorycardProps) => {
   return (
-    <div className="w-[200px] h-[200px] rounded-corner bg-green-100 flex flex-col justify-center items-center gap-3">
+    <div className="w-[200px] h-[200px] rounded-corner bg-secondary-50 flex flex-col justify-center items-center gap-3 hover:bg-secondary-100">
     <Image src={icon} alt="icon" width={100} height={100} />
-    <p className="text-black font-Poppins text-[20px] font-bold">{category}</p>
+    <p className="text-black font-sans text-[20px] font-normal">{category}</p>
   </div>
   )
 }
