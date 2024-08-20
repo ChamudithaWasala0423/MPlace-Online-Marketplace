651d3c92b45b54f7e54e606bd371210c3198ca2f
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-20 22:57:57 +0530

    Created favoritebutton

---
+++ client/src/components/ui/Favoritebutton.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React,{ useState } from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+
+
+
+const Favoritebutton:React.FC = ({}) => {
+    const [isFavorited,setFavorited]= useState(false);
+
+const clickFav=()=>{
+    setFavorited(!isFavorited);
+};
+  return (
+    <button onClick={clickFav} className="text-purple-600 text-[20px]">
+      {isFavorited ? <FaHeart /> : <FaRegHeart />}
+    </button>
+  )
+}
+
+export default Favoritebutton
