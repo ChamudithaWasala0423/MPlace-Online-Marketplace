2c1f6bfe7d3d36f0a770328783cccaee49abc056
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-20 13:16:28 +0530

    Adding fonts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        Poppins: ["var(--font-poppins)"],
+      },
+      colors:{
+        "green-green": '#f8feeb',
+        "black": '#000',
+      },
+      borderRadius: {
+        'corner': '4px',
+      },
     },
   },
   plugins: [],
