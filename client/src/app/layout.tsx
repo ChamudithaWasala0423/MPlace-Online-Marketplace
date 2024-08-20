eec8edf6a1c19b056880c3fd2b206e941c3b66f2
Tharuka Nanayakkara <tharuka.nmas@gmail.com>
2024-08-21 00:55:43 +0530

    reset font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Poppins } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: "100",
+  weight: ['100','200','300','400', '500', '600','700','800','900'],
   variable: "--font-poppins",
 });
 
