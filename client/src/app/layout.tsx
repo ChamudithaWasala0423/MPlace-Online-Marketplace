ab998756cab83c74c986e344af58de7c4161c976
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-20 16:35:30 +0530

    Adding custom font weights

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: "100",
+  weight: ['100','200','300','400', '500', '600','700','800','900'],
   variable: "--font-poppins",
 });
 
