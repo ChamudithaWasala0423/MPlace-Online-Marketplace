ce5ee5de1dc8f0f7f9307acc26899fda4cb9dff3
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-21 18:10:42 +0530

    update notification card

--- client/src/components/ui/Notifcard.tsx
+++ client/src/components/ui/Notifcard.tsx
@@ -1,37 +1,41 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
-type NotifcardProps={
-    type:string,
-    previewImage:string,
-    message:string
-}
+type NotifcardProps = {
+  type: string;
+  previewImage: string;
+  message: string;
+};
 
 const Notifcard = () => {
   return (
-    <div className='flex w-[704px] h-[99px] flex-row space-x-[20px] bg-terirary-200 hover:bg-terirary-400 rounded-corner p-3'>
-      <div className='flex w-[75px]h-[75px] overflow-hidden items-center rounded-2xl '>
+    <div className='flex w-[320px] h-[80px] flex-row space-x-[10px] bg-terirary-200 hover:bg-terirary-400 rounded-corner p-3 
+                    lg:w-[704px] lg:h-[99px] lg:space-x-[20px]'>
+      <div className='flex w-[50px] h-[50px] overflow-hidden items-center rounded-2xl 
+                      lg:w-[75px] lg:h-[75px]'>
         <Image 
-        src={"/images/chair.jpg"} 
-        width={75}
-        height={75}
-        alt={'preview image'}
-        className='flex object-cover 
-        '/>
-        </div>
-        <div className='flex w-[585px] h-[75px] ml-5 items-start gap-4'>
-          <p className=' flex self-center font-sans text-[20px] w-[125px] font-normal text-black text-balance '>
-            Ad update
-          </p>
-
-          <p className='flex self-center font-sans text-[16px] w-[463px] font-normal text-black text-pretty '>
+          src={"/images/chair.jpg"} 
+          width={50}
+          height={50}
+          alt={'preview image'}
+          className='flex object-cover w-full h-full'
+        />
+      </div>
+      
+      <div className='flex w-[240px] h-[50px] items-start gap-2 
+                      lg:w-[585px] lg:h-[75px] lg:ml-5 lg:gap-4'>
+        <p className='flex self-center font-sans text-[14px] w-[80px] font-normal text-black text-balance 
+                      lg:text-[20px] lg:w-[125px]'>
+          Ad update
+        </p>
+        
+        <p className='flex self-center font-sans text-[12px] w-[160px] font-normal text-black text-pretty 
+                      lg:text-[16px] lg:w-[463px]'>
           Your ad 'Vintage Chair' has been approved and is now live!
-          </p>
-          
-        </div>
-
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notifcard
+export default Notifcard;
