5b015db4dae2042bec2a61d3522d8dec2175d18b
Chamuditha Wasala <137607968+ChamudithaWasala0423@users.noreply.github.com>
2024-08-21 12:15:18 +0530

    chore: connect database

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 // npx tsc --init 
 import {app} from './app';
 require('dotenv').config();
+import connectDB from './utils/db';
 
 
 //create server
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
-
+    connectDB();
 });
