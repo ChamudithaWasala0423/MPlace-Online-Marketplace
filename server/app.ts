b3bd87d10476faba61b2ca111175b416576f809b
Chamuditha Wasala <137607968+ChamudithaWasala0423@users.noreply.github.com>
2024-08-21 12:49:01 +0530

    refactor: create and simplify Error Handle Utils and  CatchAsyncErrors and error handle middleware

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
 import express, { Request, Response, NextFunction } from 'express';
 export const app = express();
+import { ErrorMidleware } from './middleware/error';
 
 //body parser
 app.use(express.json({limit: '50mb'}));
 
 
+app.use(ErrorMidleware);
