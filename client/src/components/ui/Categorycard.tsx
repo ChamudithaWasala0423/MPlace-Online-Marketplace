935aaa587ef311cee76ba5739749d4b30362d486
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-21 16:19:00 +0530

    Making the category card responsive

--- client/src/components/ui/Categorycard.tsx
+++ client/src/components/ui/Categorycard.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
-type CategorycardProps = {
-  category : string
-  icon: string
+type CategorycardProps = { 
+  category: string; 
+  icon: string; 
 }
 
-
-const Categorycard = ({category,icon}:CategorycardProps) => {
+const Categorycard = ({ category, icon }: CategorycardProps) => {
   return (
-    <div className="w-[200px] h-[200px] rounded-corner bg-secondary-50 flex flex-col justify-center items-center gap-3 hover:bg-secondary-100">
-    <Image src={icon} alt="icon" width={100} height={100} />
-    <p className="text-black font-sans text-[20px] font-normal">{category}</p>
-  </div>
-  )
+    <div className="w-[200px] h-[200px] rounded-corner bg-secondary-50 flex flex-col justify-center items-center gap-3 hover:bg-secondary-100 
+                    sm:w-[115px] sm:h-[115px]">
+      <Image 
+        src={icon} 
+        alt="icon" 
+        width={100} 
+        height={100} 
+        className="sm:w-[50px] sm:h-[50px]"
+      />
+      <p className="text-black font-sans text-[20px] font-normal 
+                   sm:text-[14px]">
+        {category}
+      </p>
+    </div>
+  );
 }
 
-export default Categorycard
+export default Categorycard;
