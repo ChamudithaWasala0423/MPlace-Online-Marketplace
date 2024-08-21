f3241eb7b8bef01cf46b2b17aa3b36786d7ab3cd
Tharuka Nanayakkara <tharuka.nmas@gmail.com>
2024-08-21 16:39:00 +0530

    create search bar component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,21 @@
+'use client';  // Add this line at the top
+
 import React from 'react';
 import Button from '@/components/ui/button';
+import SearchBar from '@/components/ui/searchbar';
 
 const Page: React.FC = () => {
   return (
-    <div className="flex flex-col items-center p-8 space-y-4">
-      <Button title="View All Products" variant="primary" />
-      <Button title="Add To Cart" variant="small" />
-      <Button title="View All Products" variant="secondary" />
-    </div>
+    <>
+      <div className="flex flex-col items-center p-8 space-y-4">
+        <Button title="View All Products" variant="primary" />
+        <Button title="Add To Cart" variant="small" />
+        <Button title="View All Products" variant="secondary" />
+      </div>
+      <div className="flex flex-col items-center p-8 space-y-4">
+        <SearchBar onSearch={(value) => console.log(value)} />
+      </div>
+    </>
   );
 };
 
---
+++ client/src/components/ui/searchbar.tsx
@@ -0,0 +1,41 @@
+'use client';  // Add this line at the top
+
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
+import { FaSearch } from 'react-icons/fa'; 
+
+type SearchBarProps = {
+  onSearch: (value: string) => void;
+};
+
+const SearchBar = ({ onSearch }: SearchBarProps) => {
+  const [value, setValue] = useState('');
+
+  const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onSearch(value);
+    }
+  };
+
+  return (<>
+    <div className="relative w-1/4 h-auto p-2  rounded flex items-center">
+      <input
+        type="search"
+        placeholder="What are you looking for?"
+        className="bg-white h-10 px-4 pr-12 w-full rounded text-sm border border-transparent focus:border-[#7e2ee7] transition-colors duration-200"
+        onChange={searchHandler}
+        onKeyDown={handleKeyDown}
+        value={value}
+      />
+      <button type="submit" className="absolute right-0 top-0 bottom-2 mt-2 mr-3">
+        <FaSearch className="h-4 w-4 text-gray-600" />
+      </button>
+    </div>
+    </>
+  );
+};
+
+export default SearchBar;
