a65517e79954f7a9148ca3281a9b5655adadc169
Tharuka Nanayakkara <tharuka.nmas@gmail.com>
2024-08-22 10:14:42 +0530

    change border in text area

--- client/src/components/ui/textarea.tsx
+++ client/src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea: React.FC<TextareaProps> = ({ customWidth, customHeight, ...props
   return (
     <textarea
       className={`relative bg-neutral-100 rounded transition-colors duration-300 focus:outline-none 
-                 hover:border-[#7e2ee7] ${customWidth} ${customHeight} w-full h-full border`}
+                 hover:border-[#7e2ee7] ${customWidth} ${customHeight} w-2/4 h-full border border-2` }
       /*style={{
         width: customWidth || '1123px',
         height: customHeight || '50px',
