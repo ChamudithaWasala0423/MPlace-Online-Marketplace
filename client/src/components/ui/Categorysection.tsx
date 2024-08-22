6966b369929513409c726e8ac68b95936bebad79
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-22 21:34:59 +0530

    changing icon

--- client/src/components/ui/Categorysection.tsx
+++ client/src/components/ui/Categorysection.tsx
@@ -12,15 +12,15 @@ type Props = {
 };
 
 const defaultCategories: Category[] = [
-  { id: 1, name: "Electronics", icon: "/images/electronincs.png" },
-  { id: 2, name: "Electronics", icon: "/images/electronincs.png" },
-  { id: 3, name: "Electronics", icon: "/images/electronincs.png" },
+  { id: 1, name: "Electronics", icon: "/images/electronics.png" },
+  { id: 2, name: "Electronics", icon: "/images/electronics.png" },
+  { id: 3, name: "Electronics", icon: "/images/electronics.png" },
 ];
 
 const Categorysection: React.FC<Props> = ({ categories = defaultCategories }) => {
   return (
-    <section className="w-full flex flex-col items-center justify-center py-16 px-8 bg-gray-50">
-      <h2 className="text-3xl font-bold mb-12 text-center text-black">
+    <section className="w-full flex flex-col items-center justify-center py-8 px-8 bg-gray-50">
+      <h2 className="text-3xl font-bold font-sans mb-12 text-center text-black">
         Categories
       </h2>
       <div className="flex flex-wrap items-center justify-center gap-8">
