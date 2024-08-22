cbd75d037ce9541243210a25f5e6d95d7e3801bd
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-22 18:17:00 +0530

    Adding default values to category section

--- client/src/components/ui/Categorysection.tsx
+++ client/src/components/ui/Categorysection.tsx
@@ -2,17 +2,26 @@ import React from 'react'
 import Categorycard from './Categorycard'
 
 type Category={
-    id: number;
-    name: string;
-    icon: string;
+    id?: number;
+    name?: string;
+    icon?: string;
 }
 
 type Props={
-    categories:Category[];
+    categories?:Category[];
 }
+
+const defaultCategories: Category[] = [
+    { id: 1, name: 'Electronics', icon: '/images/electronincs.png' },
+    { id: 2, name: 'Electronics', icon: '/images/electronincs.png' },
+    { id: 3, name: 'Electronics', icon: '/images/electronincs.png' },
+];
+
 const Categorysection:React.FC<Props> = ({categories}) => {
   return (
-    <div>Categorysection</div>
+    <section className="w-full flex flex-col items-center justify-center py-16 px-8 bg-gray-50">
+
+    </section>
   )
 }
 
