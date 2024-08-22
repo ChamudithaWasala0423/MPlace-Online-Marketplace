10cf303fac03e4922e46dd817020125d642cf445
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-22 21:46:35 +0530

    Adding types and props

--- client/src/components/ui/ItemsSection.tsx
+++ client/src/components/ui/ItemsSection.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Itemcard from "./Itemcard";
 
+type Item = {
+  id?: number;
+  name?: string;
+  description?: string;
+  price?: string;
+  location?: string;
+  daysAdded?: number;
+  itemImage?: string;
+};
+
+type Props = {
+    items?: Item[];
+  };
 const ItemsSection = () => {
-  return (
-    <div>ItemsSection</div>
-  )
-}
+  return <div>ItemsSection</div>;
+};
 
-export default ItemsSection
+export default ItemsSection;
