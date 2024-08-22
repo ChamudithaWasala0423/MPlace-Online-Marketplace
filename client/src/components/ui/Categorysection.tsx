1467dbd0f21336b51f69d36cb039c2097da54e0a
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-22 17:45:00 +0530

    Create category card

---
+++ client/src/components/ui/Categorysection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Categorycard from './Categorycard'
+
+const Categorysection = () => {
+  return (
+    <div>Categorysection</div>
+  )
+}
+
+export default Categorysection
