86a326d412d1a94e8528b2bcfe1e2d82fbf69d90
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-24 02:01:25 +0530

    map itemcard props

--- client/src/app/dashboard/draftsad/page.tsx
+++ client/src/app/dashboard/draftsad/page.tsx
@@ -24,7 +24,14 @@ const DraftsAd: React.FC<Props> = ({drafts=defaultAds}) => {
   return (
     <div className="w-full relative flex flex-row items-center justify-center flex-wrap content-center py-9 px-0 box-border gap-6">
         {drafts.map((item) => (
-          
+          <Itemcard
+            key={item.id}
+            name={item.name || "Default Category"}
+            description={item.description || "Default description"}
+            price={item.price || "Default price"}
+            location={item.location || "Default location"}
+            daysAdded={item.daysAdded || 0}
+            itemImage={item.itemImage || "/images/chair.jpg"}/>
         ))}
     </div>
   )
