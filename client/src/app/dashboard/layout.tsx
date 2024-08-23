1823c182ad794357b338339c15f084e08534e435
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-23 17:41:12 +0530

    Styling dashboard layout

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -2,17 +2,24 @@
 
 import React from "react";
 import Sidebar from "@/components/ui/Sidebar";
+import Navbar from "@/components/ui/Navbar";
 
-const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
-    <div className="flex flex-col lg:grid lg:grid-cols-sidebar-main lg:gap-[74px] p-4 bg-background-400">
-      {/* Sidebar */}
-      <Sidebar />
+    <div className="bg-background-400">
+      <Navbar></Navbar>
+      <div className="flex px-4 lg:px3-2 align- w-full justify-end bg-background-400 text-black font-sans font-normal lg:my-20 sm:my-10 ">Welcome </div>
+      <div className="flex flex-col lg:grid lg:grid-cols-sidebar-main lg:gap-[74px]  bg-background-400 lg:px-32 px-8">
 
-      {/* Main Content */}
-      <main className="lg:col-start-2 lg:col-end-3 flex-1 bg-background-400 p-6 shadow-md">
-        {children}
-      </main>
+        <Sidebar />
+
+        {/* Main Content */}
+        <main className="lg:col-start-2 lg:col-end-3 flex-1 bg-background-400  shadow-md font-sans">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
