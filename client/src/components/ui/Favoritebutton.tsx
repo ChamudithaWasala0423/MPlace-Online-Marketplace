5075bc195e64f3d7079491f75e85b7adf0ef7d82
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-23 12:33:37 +0530

    Aligned code

--- client/src/components/ui/Favoritebutton.tsx
+++ client/src/components/ui/Favoritebutton.tsx
@@ -1,20 +1,18 @@
 "use client";
-import React,{ useState } from 'react'
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
+const Favoritebutton: React.FC = ({}) => {
+  const [isFavorited, setFavorited] = useState(false);
 
-
-const Favoritebutton:React.FC = ({}) => {
-    const [isFavorited,setFavorited]= useState(false);
-
-const clickFav=()=>{
+  const clickFav = () => {
     setFavorited(!isFavorited);
-};
+  };
   return (
     <button onClick={clickFav} className="text-purple-600 text-[20px]">
       {isFavorited ? <FaHeart /> : <FaRegHeart />}
     </button>
-  )
-}
+  );
+};
 
-export default Favoritebutton
+export default Favoritebutton;
