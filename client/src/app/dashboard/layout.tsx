c148b64148e168fb4d46be78c48b01f4341ab589
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-23 12:39:11 +0530

    Adding layout styling for dashboard

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -5,7 +5,15 @@ import Sidebar from "@/components/ui/Sidebar";
 
 const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    
+    <div className="flex flex-col lg:grid lg:grid-cols-sidebar-main lg:gap-[74px] p-4 bg-background-400">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <main className="lg:col-start-2 lg:col-end-3 flex-1 bg-background-400 p-6 shadow-md">
+        {children}
+      </main>
+    </div>
   );
 };
 
