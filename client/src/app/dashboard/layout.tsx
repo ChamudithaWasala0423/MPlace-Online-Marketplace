6995db3210ab4b2737ffa470e656dea7a40857f7
ThilPerera <130539278+ThilSenadi@users.noreply.github.com>
2024-08-23 12:37:27 +0530

    Create layout for dashboard

---
+++ client/src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import Sidebar from "@/components/ui/Sidebar";
+
+const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    
+  );
+};
+
+export default DashboardLayout;
