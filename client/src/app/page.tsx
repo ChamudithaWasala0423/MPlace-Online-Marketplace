82b1427bf839a8055b63f438cfa9f55093b4f39d
Chamuditha Wasala <137607968+ChamudithaWasala0423@users.noreply.github.com>
2024-08-28 12:52:50 +0530

    fix: resolving conflicts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import Footer from "../components/ui/footer";
-import globals from "globals";
-import Navbar from "../components/ui/navbar";
-import Button from "../components/ui/button";
-import SearchBar from "../components/ui/searchbar";
-import Contact from "@/pages/contact";
-import Textarea from "@/components/ui/textarea"
-import ProductDetails from "@/components/ui/productDetails";
-import About from "@/pages/about";
-import ItemDisplaySmall from "@/components/ui/itemDisplaySmall";
-import FeaturedAdvertisement from "@/pages/featuredAdvertisement";
-import Category from "@/pages/category";
-import RecentlyAdded from "@/pages/recentlyAdded";
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div >
-      <RecentlyAdded />
-    </div>
-  );
-}
+const Home: React.FC = () => {
+    return (
+        <></>
+    );
+};
+
+export default Home;
