3f0b46b3541ae32148c53d4abee5363d16a8db46
Chamuditha Wasala <137607968+ChamudithaWasala0423@users.noreply.github.com>
2024-08-31 20:07:02 +0530

    fix: UI errors

--- client/src/components/sections/ItemsSection.tsx
+++ client/src/components/sections/ItemsSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import Itemcard from "../ui/itemcard";
+import Itemcard from "@/components/ui/itemcard";
 import Link from "next/link";
 import { useGetAllAdsQuery } from "@/redux/features/ads/adsApi";
 
