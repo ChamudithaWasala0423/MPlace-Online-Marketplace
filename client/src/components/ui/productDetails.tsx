81b60b0b3da2fec54d32508955c9ceed42ca9bb8
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2024-09-03 07:49:54 +0530

    fix: UI errors

--- client/src/components/ui/productDetails.tsx
+++ client/src/components/ui/productDetails.tsx
@@ -1,6 +1,5 @@
-import { FunctionComponent } from 'react';
-import Rating from '../ui/rating';
-import { FaMapMarkerAlt } from 'react-icons/fa'; // Import location icon from react-icons
+import { FunctionComponent } from "react";
+import { FaMapMarkerAlt } from "react-icons/fa"; // Import location icon from react-icons
 
 interface ProductDetailsProps {
   productName?: string;
@@ -29,7 +28,9 @@ const ProductDetails: FunctionComponent<ProductDetailsProps> = ({
     <div className="w-full flex flex-col items-start justify-start p-4 text-left text-black font-sans">
       <div className="flex flex-col items-start justify-start w-full lg:w-[407px] gap-4">
         <div className="w-full flex flex-col gap-2">
-          <h1 className="text-2xl font-semibold tracking-wide">{productName}</h1>
+          <h1 className="text-2xl font-semibold tracking-wide">
+            {productName}
+          </h1>
           <p className="text-sm leading-6">{description}</p>
           <div className="text-xl font-bold">{price}</div>
         </div>
@@ -38,14 +39,16 @@ const ProductDetails: FunctionComponent<ProductDetailsProps> = ({
           <span>{location}</span>
         </div>
       </div>
-      
+
       <div className="w-full lg:w-[407px] mt-6">
         <hr className="opacity-50" />
       </div>
 
       <div className="flex flex-col w-full lg:w-[407px] mt-6 gap-6">
         <div>
-          <h2 className="text-lg font-medium text-black/50">Instructions to Buyers</h2>
+          <h2 className="text-lg font-medium text-black/50">
+            Instructions to Buyers
+          </h2>
           <p className="text-sm leading-6">{instructionsToBuyers}</p>
         </div>
         <div>
@@ -53,7 +56,7 @@ const ProductDetails: FunctionComponent<ProductDetailsProps> = ({
           <p className="text-sm leading-6">{category}</p>
         </div>
       </div>
-      
+
       <div className="w-full lg:w-[407px] mt-6">
         <hr className="opacity-50" />
       </div>
@@ -64,7 +67,6 @@ const ProductDetails: FunctionComponent<ProductDetailsProps> = ({
           {/* <img className="w-8 h-8 rounded-full" alt="seller" src={sellerImage} /> */}
           <span className="text-[14px]">{seller}</span>
           <div className="border-r border-black/50 h-5 mx-2" />
-          <Rating rating={sellerRating} />
         </div>
       </div>
     </div>
