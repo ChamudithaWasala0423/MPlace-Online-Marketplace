dcd8e4605727ba5a35887ede04618816be1279c6
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2024-09-03 07:47:22 +0530

    Fix: UI Errors

--- client/src/app/dashboard/postAd/page.tsx
+++ client/src/app/dashboard/postAd/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Textarea from "@/components/ui/textarea";
 import InputArea from "@/components/ui/inputarea";
 import { useCreateAdsMutation } from "@/redux/features/ads/adsApi";
 import toast from "react-hot-toast";
@@ -290,7 +289,7 @@ const EditAddPage: React.FC = () => {
                 know.
               </p>
               <div className="text-black">
-                <Textarea
+                <InputArea
                   customWidth="w-full"
                   customHeight="h-32"
                   name="description"
