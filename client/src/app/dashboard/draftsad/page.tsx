1243b75e0e973b3485f11389bd70315259652755
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2024-09-03 07:42:27 +0530

    fix: UI Errors

--- client/src/app/dashboard/draftsad/page.tsx
+++ client/src/app/dashboard/draftsad/page.tsx
@@ -1,15 +1,12 @@
-
-import Itemcard from '@/components/ui/itemcard';
-import Link from 'next/link';
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
 
 const DraftsAd = () => {
   return (
     <div className="w-full relative flex flex-row items-center justify-center flex-wrap content-center py-9 px-0 box-border gap-6 ">
-    <h1 className='text-black font-bold'>No Draft Ad Found</h1>
+      <h1 className="text-black font-bold">No Draft Ad Found</h1>
     </div>
-  )
-}
+  );
+};
 
 export default DraftsAd;
